f30e986a0989292138f1dacfabb88054db16e882
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-04-09 17:37:57 +0600

    added a show button in password

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     <div className="navbar bg-base-100 md:mt-4">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className=" lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
@@ -26,7 +26,7 @@ const Navbar = () => {
             <li><a>Update Profile</a></li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">CozyNest</a>
+        <a className="text-xl md:text-4xl font-bold ml-2">CozyNest</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 gap-8">
@@ -61,7 +61,7 @@ const Navbar = () => {
 
         {
           user ? <>
-          <div className="w-10 rounded-full dropdown dropdown-hover">
+          <div className="w-10 h-10 rounded-full flex justify-center items-center dropdown dropdown-hover">
             <img className="rounded-full" src={user.photoURL} />
             <div tabIndex={0} role="button" className=" m-1"></div>
             <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow  rounded-box w-52 bg-gray-300">
--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,13 +1,16 @@
-import { useContext} from "react";
+import { useContext, useState} from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Context/Context";
 import toast, { Toaster } from 'react-hot-toast';
 import { Helmet } from "react-helmet-async";
+import { FiEye } from "react-icons/fi";
+import { FiEyeOff } from "react-icons/fi";
 
 
 const Register = () => {
     
     let {createUser} = useContext(AuthContext)
+    let [show, setShow] = useState(false)
     let handleRegister =(e) => {
         e.preventDefault()
         let from = new FormData(e.currentTarget)
@@ -39,8 +42,6 @@ const Register = () => {
           });
 
         }
-        // {Error && toast.error(Error) }
-    
     return (
         <div>
             <Helmet>
@@ -54,8 +55,11 @@ const Register = () => {
           <input type="text" name="name" placeholder="Your Name" className="rounded-none border-b-[1px] border-gray-300 w-full pb-2 mb-5"/>
           <input type="text" name="photoURL" placeholder="Photo URL" className="rounded-none border-b-[1px] border-gray-300 w-full pb-2 mb-5"/>
           <input type="email" name="email" placeholder="Email" className="rounded-none border-b-[1px] border-gray-300 w-full pb-2 mb-5" required />
-          <input type="password" name="password" placeholder="password" className=" rounded-none border-b-[1px] border-gray-300 w-full pb-2" required />
-          <input   className="btn mt-2 bg-[#64ade8] text-white w-full" type="submit" value="Login" />
+          <div className="flex relative">
+          <input type={show ? "text" : "password"} name="password" placeholder="password" className="rounded-none border-b-[1px] border-gray-300 w-full pb-2" required />  
+          <span onClick={()=> setShow(!show)} className="absolute  right-2">{show ? <FiEye className="text-[20px] text-gray-500"/> : <FiEyeOff className="text-[20px] text-gray-500"/>}</span >
+          </div>
+          <input className="btn mt-2 bg-[#64ade8] text-white w-full" type="submit" value="Login" />
           <Toaster />
           <p className="text-[14px] mt-3 font-semibold text-center">Already Have an Account ? <Link to="/login" className="text-blue-500 underline">Lgin</Link></p>
       </form>
